1f32a4303384215110e1175f35e34f95f66ffbe9
Dzianis <somniummisellus@gmail.com>
2020-12-13 21:49:44 +0300

    silence

--- app/controllers/api/dialogs_controller.rb
+++ app/controllers/api/dialogs_controller.rb
@@ -4,10 +4,15 @@ class Api::DialogsController < ApplicationController
         user = User.find_by_token(token)
         dialogs = []
         UsersToDialog.where(user_id: user.id).each do |dialog_id|
-            #dialog = Dialog.find_by_id(dialog_id).to_json(:only => [:id])
-            UsersToDialog.where(dialog_id:dialog_id)
+            dialog = JSON.parse Dialog.find_by_id(dialog_id).to_json(:only => [:id])
+            another_user_id = UsersToDialog.where(dialog_id: dialog_id).reject { |c| c.user_id == user.id }[0]
+            another_user = User.find_by_id(another_user_id)
+            dialog[:another_user_name] = another_user.name
+            dialog[:another_user_surname] = another_user.surname
+            photo = Photo.find_by_id(another_user.photo_id)
+            dialog[:another_user_photo] = photo.photo
             dialogs.push(dialog)
         end
-
+        render json: dialogs, status: 200
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   #
 
-  config.web_socket_server_url = "wss://chess-network.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://chess-network.herokuapp.com', 'http://chess-network.herokuapp.com']
+  config.web_socket_server_url = 'wss://chess-network.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = %w[https://chess-network.herokuapp.com /http:\/\/chess-network.herokuapp.com.*/]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
